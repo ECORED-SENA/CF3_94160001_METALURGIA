930d25f87f2232716ed5fa1b2d0e7bb59883c489
emilydev <emilse.a.b@gmail.com>
2024-11-11 18:47:43 -0500

    se carga sintesis

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,12 +2,12 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p.mb-5 A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
+    p.mb-5 A continuación, se presenta una síntesis de la temática estudiada en el componente formativo.  
 
     .row.justify-content-center
       .col-lg-11.mb-5
         figure.bg-color-sintesis.p-5.brounded
-          img(src="@/assets/curso/sintesis.svg", alt="El diagrama presenta  un conjunto de medidas de bioseguridad en acuicultura para evitar la introducción y propagación de patógenos en los cultivos acuícolas. Se detallan las causas de las enfermedades, que incluyen factores biológicos, físico-químicos, nutricionales y denso-dependientes. Los métodos de intervención abarcan el registro de campo, el manejo adecuado de los organismos y el control de parámetros físico-químicos. La normatividad menciona el cumplimiento de normas, como la Resolución 2424 de 2009 (INCODER), mientras que la prevención incluye medidas como cuarentenas y uso de filtros. Finalmente, la seguridad laboral se enfoca en la identificación de riesgos en las zonas de trabajo acuícolas.")
+          img(src="@/assets/curso/sintesis.svg", alt="La síntesis presenta el proceso siderúrgico, comenzando con la extracción de hierro, que es triturado y sinterizado antes de ingresar al alto horno. Este utiliza carbón coque como combustible y caliza como fundente, produciendo arrabio y gases de alto horno. El arrabio se carga en el horno convertidor junto con oxígeno y fundentes para obtener acero, mientras que la escoria es un subproducto. La chatarra también se introduce en el proceso a través de un horno eléctrico para ajustar la aleación. El acero final se lamina para producir perfiles estructurales, láminas, platinas y alambres.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
